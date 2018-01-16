0ba3dd42fec74c1b1766a502e7b3ac2235b13e5a
Joonas Leonsaari <joonas.leonsaari@gmail.com>
2018-01-16 14:52:11 +0200

    Task1 completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,21 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { FooterComponent } from './footer/footer.component';
+import { MainComponent } from './main/main.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavigationComponent,
+    FooterComponent,
+    MainComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule
